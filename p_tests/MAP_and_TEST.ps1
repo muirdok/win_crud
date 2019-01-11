872fad68f4b4b0c13bec0ad4a997ad6a46b06730
Georgy McD <muirdok@gmail.com>
2019-01-11 13:27:58 +0300

    user check

--- p_tests/MAP_and_TEST.ps1
+++ p_tests/MAP_and_TEST.ps1
@@ -12,7 +12,8 @@ $folder_name = "testfolder"
 
 #Atach disk
 #New-PSDrive -Persist -Name $disk_name -PSProvider "FileSystem" -Root $unc_path -Credential $user_credential
-net use $disk_name $unc_path /user:$user_name $user_password
+#net use $disk_name $unc_path /user:$user_name $user_password
+net use $disk_name $unc_path
 Start-Sleep -m 500
 
 #Create file and directory
